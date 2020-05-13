47646958ac6d8b3e484912aaf906fe2021701b90
Tsundere Chen <me@tsunderechen.io>
2020-05-13 18:09:00 +0000

    Update files

--- copyFileToRepo.sh
+++ copyFileToRepo.sh
@@ -3,6 +3,7 @@
 rm -Rf ../mlgame-pingpong-ml-bot/saved_model
 cp ./training.py ../mlgame-pingpong-ml-bot
 cp ./validation.py ../mlgame-pingpong-ml-bot
+cp ./training.ipynb ../mlgame-pingpong-ml-bot
 cp -r ./log/* ../mlgame-pingpong-ml-bot/log
 cp -r ./saved_model ../mlgame-pingpong-ml-bot
 cp -r ./games/pingpong/ml/ml_play_NN.py ../mlgame-pingpong-ml-bot
